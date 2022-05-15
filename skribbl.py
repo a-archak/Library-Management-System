30a27e9df51c8880d7b1f621e206fc28e9df6ba4
Archak <80565313+a-archak@users.noreply.github.com>
2022-05-15 12:27:27 +0545

    Update skribbl.py

--- skribbl.py
+++ skribbl.py
@@ -1,3 +1,9 @@
+#This program displays the possible keywords that you're supposed to guess in skribbl.io 
+#First put the custom keywords in the array
+#Then program asks to enter the number of characters in word that you're trying to guess
+#REMEMBER SPACE COUNTS if the word is "____ ___" Then number of characters should be 8
+#Then program displays every possible keyword that fits the requirement from array.
+
 # list keywords of scribble here in array
 keywords = ["lion", "fish", "Ferret", "goat", "crab", "wolverine", "cow", "horse", "crow", "meerkat", "seal", "llama", "leopard", "rhinoceros", "warthog", "ape", "chipmunk", "elephant", "deer", "walrus", "panda", "pig", "parakeet", "beaver", "puma", "bison", "Eagle", "owl", "fox", "anteater", "groundhog", "lamb", "parrot", "panther", "coyote", "koala", "ram", "Squirrel", "moose", "Kitten", "Otter", "octopus", "iguana", "sheep", "dog", "giraffe", "camel", "kangaroo", "toad", "Tiger", "sloth", "raccoon", "alligator", "weasel", "rooster", "jaguar", "polar bear", "snake", "wombat", "bear", "wolf", "chameleon", "hedgehog", "newt", "donkey", "salamander", "mole", "bat", "Gorilla", "skunk", "rabbit", "pony", "chimpanzee", "whale", "opossum", "monkey", "platypus", "buffalo", "zebra", "dolphin", "cat", "bull", "rat", "chinchilla", "reindeer", "armadillo", "Antelope", "turtle", "puppy", "hippopotamus", "porcupine", "starfish", "hamster", "crocodile", "chicken", "lemur", "bumble bee", "ox", "bald eagle", "cheetah", "frog", "orangutan", "lizard", "yak", "mouse", "duck", "seahorse", "penguin", "ant", "spider", "ladybug", "butterfly", "worm", "dragonfly", "flamingo", "jellyfish", "moth", "ostrich", "peacock", "shark"]
 
